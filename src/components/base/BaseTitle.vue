df58d8b7c2a73791353d7f72ada17b7e3984684d
Muhammed Ali Boztepe <aliboztepee1@gmail.com>
2023-07-28 14:23:18 +0300

    base-title added.

---
+++ src/components/base/BaseTitle.vue
@@ -0,0 +1,52 @@
+<template>
+    <p :class="typical">
+        <slot></slot>
+    </p>
+</template>
+
+<script>
+export default {
+    name: "BaseTitle",
+    props: {
+        typical: {
+            type: String,
+            required: false,
+            default: "title"
+        },
+    },
+    data() {
+        return {
+        }
+    },
+}
+</script>
+
+<style lang="scss" scoped>
+.title {
+    font-family: $secondary-font;
+    font-weight: bold;
+    position: relative;
+    font-size: 34px;
+
+    &.-size {
+        font-size: 28px;
+    }
+
+    &.-psuedo {
+        &::before {
+            position: absolute;
+            content: "";
+            top: 3px;
+            left: -15px;
+            width: 6px;
+            height: 28px;
+            border-radius: 10px;
+            background-color: $electricViolet;
+        }
+    }
+
+    &.-fsmall{
+        font-size: 28px;
+    }
+}
+</style>
