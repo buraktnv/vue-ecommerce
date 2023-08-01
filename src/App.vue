8b656d37ad66d44980ebb3fac0030bed8c602525
Yavuz Savan <isminivermeyenyavuz@gmail.com>
2023-08-01 12:01:12 +0300

    discount-card init

--- src/App.vue
+++ src/App.vue
@@ -5,16 +5,24 @@
       <router-link to="/about">About</router-link>
     </nav>
     <router-view />
+    <DiscountCard :backgroundImage="imgURL" :subtitle="subtitle" :title="title" :discount="discount" :explore="explore"></DiscountCard>
   </div>
 </template>
 
 <script>
+
+import DiscountCard from "./components/DiscountCard";
+
 export default {
   name: "app",
-  components: {},
+  components: { DiscountCard, },
   data() {
     return {
       value1: null,
+      subtitle: "Low Price",
+      title: "High Coziness",
+      discount: "UPTO 50% OFF",
+      explore: "Explore Items",
     };
   },
 };
---
+++ src/components/DiscountCard/index.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="discount" :style="{ backgroundImage: 'url('+ backgroundImage +')'}">
+        <base-text>{{ subtitle }}</base-text>
+        <base-title>{{ title }}</base-title>
+        <base-text>{{ discount }}</base-text>
+        <base-text>{{ explore }}</base-text> <!-- make hyperlink component ? -->
+    </div>
+</template>
+
+<script>
+import BaseText from '../base/BaseText.vue';
+import BaseTitle from '../base/BaseTitle.vue';
+
+export default {
+  components: { BaseText, BaseTitle },
+    name: "DiscountCard",
+    props:{
+        backgroundImage: String,
+        subtitle: String,
+        title: String,
+        discount: String,
+        explore: String,
+    },
+    data() {
+        return {
+
+        }
+    },
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
